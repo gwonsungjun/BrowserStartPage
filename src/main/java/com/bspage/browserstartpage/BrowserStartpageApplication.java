6187565525a651201729887a0c3d78da0844f266
권성준 <sungjunpizz@gmail.com>
2018-04-18 22:38:12 +0900

    Create Project

---
+++ src/main/java/com/bspage/browserstartpage/BrowserStartpageApplication.java
@@ -0,0 +1,12 @@
+package com.bspage.browserstartpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowserStartpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowserStartpageApplication.class, args);
+	}
+}
